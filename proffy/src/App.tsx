1d07bed25c82d922f75cdb78d2e47473458d46ec
henriq <henriquesqs@hotmail.com>
2020-08-04 00:13:49 -0300

    Created Header component

--- proffy/src/App.tsx
+++ proffy/src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
+import Routes from "./routes";
 import "./assets/styles/global.css";
-import Landing from "./pages/landing";
 
 function App() {
-    return <Landing />;
+    return <Routes />;
 }
 
 export default App;
---
+++ proffy/src/pages/TeacherList/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import PageHeader from "../../components/PageHeader";
+
+function TeacherList() {
+    return (
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis." />
+        </div>
+    );
+}
+
+export default TeacherList;
