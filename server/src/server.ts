5ee5bf853a10c3bee29e83fb10c92d2132e79e13
henriq <henriquesqs@hotmail.com>
2020-08-04 22:20:55 -0300

    allowing our server to communicate with our frontend from diff ports

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes";
 
 const app = express(); // creating express application
 app.use(express.json()); // express must undedstand json
 app.use(routes);
+app.use(cors());
 
 app.listen(3333); // listening to http requests on port 3333 (access via localhost:3333)
 
