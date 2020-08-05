743efbef7466c6a3a0f2ce9a4e8956f93fa63e3b
henriq <henriquesqs@hotmail.com>
2020-08-05 18:39:23 -0300

    finalized frontend

--- proffy/src/pages/TeacherForm/index.tsx
+++ proffy/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
@@ -9,6 +9,26 @@ import Select from "../../components/Select";
 // TeacherForm component is responsible for showing teachers a form so they can
 // answer some questions about himself/ sheself and the lecture.
 function TeacherForm() {
+    // Creating a state to allow teachers to add new schedules
+    const [scheduleItems, setScheduleItems] = useState([
+        {
+            week_day: 0,
+            from: "",
+            to: "",
+        },
+    ]);
+
+    function addNewScheduleItem() {
+        setScheduleItems([
+            ...scheduleItems,
+            {
+                week_day: 0,
+                from: "",
+                to: "",
+            },
+        ]);
+    }
+
     return (
         <div id="page-teacher-form" className="container">
             <PageHeader
@@ -41,10 +61,47 @@ function TeacherForm() {
                             { value: "Português", label: "Português" },
                             { value: "Inglês", label: "Inglês" },
                             { value: "História", label: "História" },
-                            { value: "Educação Física", label: "Educação Física" },
+                            {
+                                value: "Educação Física",
+                                label: "Educação Física",
+                            },
                         ]}
                     />
                     <Input name="cost" label="Custo da sua hora por aula" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        Horários disponíveis
+                        <button type="button" onClick={addNewScheduleItem}>
+                            + Novo horário
+                        </button>
+                    </legend>
+
+                    {scheduleItems.map((scheduleItem) => {
+                        return (
+                            <div
+                                key={scheduleItem.week_day}
+                                className="schedule-item"
+                            >
+                                <Select
+                                    name="week-day"
+                                    label="Dia da semana"
+                                    options={[
+                                        { value: "0", label: "Domingo" },
+                                        { value: "1", label: "Segunda-feira" },
+                                        { value: "2", label: "Terça-feira" },
+                                        { value: "3", label: "Quarta-feira" },
+                                        { value: "4", label: "Quinta-feira" },
+                                        { value: "5", label: "Sexta-feira" },
+                                        { value: "6", label: "Sábado" },
+                                    ]}
+                                />
+                                <Input name="from" label="Das" type="time" />
+                                <Input name="to" label="Até" type="time" />
+                            </div>
+                        );
+                    })}
                 </fieldset>
 
                 <footer>
