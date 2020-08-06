d9bcf02ca30b31d7c097d4c4a2dbc45a51dee9cc
henriq <henriquesqs@hotmail.com>
2020-08-06 20:51:20 -0300

    finished landing page without routes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,37 @@
 import {StatusBar} from "expo-status-bar";
 import React from "react";
 import {StyleSheet, Text, View} from "react-native";
+import Landing from "./src/pages/Landing/Index";
+import {AppLoading} from "expo";
+
+import {
+    useFonts,
+    Archivo_400Regular,
+    Archivo_700Bold,
+} from "@expo-google-fonts/archivo";
+import {
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+} from "@expo-google-fonts/poppins";
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Hello, world!!!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
-}
+    // Declaring fonts we're going to use
+    let [fontsLoaded] = useFonts({
+        Archivo_400Regular,
+        Archivo_700Bold,
+        Poppins_400Regular,
+        Poppins_600SemiBold,
+    });
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    } else {
+        return (
+            // Creating a fragment in order to export Landing and StatusBar together
+            <>
+                <Landing />
+                <StatusBar style="light" />
+            </>
+        );
+    }
+}
---
+++ mobile/src/@types/index.d.ts
@@ -0,0 +1,1 @@
+declare module "*.png"; // allowing png files import
