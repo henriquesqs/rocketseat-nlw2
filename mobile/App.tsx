da4c8b8dd1960933f0f0a093fa47d1a629818ffa
henriq <henriquesqs@hotmail.com>
2020-08-06 23:54:32 -0300

    creating routes of our app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,6 @@
 import {StatusBar} from "expo-status-bar";
 import React from "react";
 import {StyleSheet, Text, View} from "react-native";
-import Landing from "./src/pages/Landing/Index";
 import {AppLoading} from "expo";
 
 import {
@@ -13,6 +12,7 @@ import {
     Poppins_400Regular,
     Poppins_600SemiBold,
 } from "@expo-google-fonts/poppins";
+import AppStack from "./src/routes/AppStack";
 
 export default function App() {
     // Declaring fonts we're going to use
@@ -29,7 +29,7 @@ export default function App() {
         return (
             // Creating a fragment in order to export Landing and StatusBar together
             <>
-                <Landing />
+                <AppStack />
                 <StatusBar style="light" />
             </>
         );
