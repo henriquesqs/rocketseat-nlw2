c728475223b114f7d6ff6da0a51a9f6878e5e446
henriq <henriquesqs@hotmail.com>
2020-08-06 23:17:33 -0300

    Updates

---
+++ precache-manifest.4617b97650f26f0bb59eb9792d66ee8c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbeedaa49a8cfe2a78ab3779bb393a4b",
+    "url": "/rocketseat-nlw2/index.html"
+  },
+  {
+    "revision": "b68c960836fb9eaf9cd7",
+    "url": "/rocketseat-nlw2/static/css/main.9699500e.chunk.css"
+  },
+  {
+    "revision": "abfb26b5c863071fe42d",
+    "url": "/rocketseat-nlw2/static/js/2.c8a7d12a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rocketseat-nlw2/static/js/2.c8a7d12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68c960836fb9eaf9cd7",
+    "url": "/rocketseat-nlw2/static/js/main.1b9e60c3.chunk.js"
+  },
+  {
+    "revision": "aa02c4536091c179212f",
+    "url": "/rocketseat-nlw2/static/js/runtime-main.9f734b65.js"
+  },
+  {
+    "revision": "dc8c47fa945838a208074157d997436d",
+    "url": "/rocketseat-nlw2/static/media/back.dc8c47fa.svg"
+  },
+  {
+    "revision": "156102ee75a899123483211bbf4a8fbd",
+    "url": "/rocketseat-nlw2/static/media/give-classes.156102ee.svg"
+  },
+  {
+    "revision": "211f2bb00860f91804cfb2354e17b255",
+    "url": "/rocketseat-nlw2/static/media/landing.211f2bb0.svg"
+  },
+  {
+    "revision": "86fef799b0726c66fb7a9d710e2d922c",
+    "url": "/rocketseat-nlw2/static/media/logo.86fef799.svg"
+  },
+  {
+    "revision": "bfdb3ebe513b5b2411a5014d6aad3abe",
+    "url": "/rocketseat-nlw2/static/media/purple-heart.bfdb3ebe.svg"
+  },
+  {
+    "revision": "e7a7e1b4e016245fa97e6f42aee4bcaa",
+    "url": "/rocketseat-nlw2/static/media/study.e7a7e1b4.svg"
+  },
+  {
+    "revision": "6dfaa2f94fc54e53666a511b779f90b4",
+    "url": "/rocketseat-nlw2/static/media/warning.6dfaa2f9.svg"
+  },
+  {
+    "revision": "45c6e6ecebde5ee441644d41cc132ce4",
+    "url": "/rocketseat-nlw2/static/media/whatsapp.45c6e6ec.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rocketseat-nlw2/tree/master/web/src/precache-manifest.c24d2c7edf1ede607e3187e9a941c2e9.js"
+  "/rocketseat-nlw2/precache-manifest.4617b97650f26f0bb59eb9792d66ee8c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rocketseat-nlw2/tree/master/web/src/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rocketseat-nlw2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
