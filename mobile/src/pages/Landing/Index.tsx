f28c90b144db37795af8877ffeae5b55a1518991
henriq <henriquesqs@hotmail.com>
2020-08-08 12:37:04 -0300

    Testing ignore-paths

--- mobile/src/pages/Landing/Index.tsx
+++ mobile/src/pages/Landing/Index.tsx
@@ -35,7 +35,7 @@ function Landing() {
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
             <Text style={styles.title}>
-                Bem-vindo, {"\n"}
+                Bem-viindo, {"\n"}
                 <Text style={styles.titleBold}>O que deseja fazer?</Text>
             </Text>
 
