9b868a69980c5517093c681a6e48d514ef8d9975
henriq <henriquesqs@hotmail.com>
2020-08-07 21:20:11 -0300

    Changed BrowserRouter to HashRouter

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import {HashRouter, Route} from "react-router-dom";
 import Landing from "./pages/Landing";
 import TeacherList from "./pages/TeacherList";
 import TeacherForm from "./pages/TeacherForm";
@@ -7,14 +7,14 @@ import TeacherForm from "./pages/TeacherForm";
 // Creating Routes component to our SPA
 function Routes() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             {/* Route to our index page (landing page) */}
             <Route path="/" exact component={Landing} />{" "}
             {/* Route to our "/study" page */}
             <Route path="/study" component={TeacherList} />{" "}
             {/* Route to our "give-classes" page */}
             <Route path="/give-classes" component={TeacherForm} />{" "}
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
