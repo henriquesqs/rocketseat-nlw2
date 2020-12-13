cd3daff88304db4ecb495e5e4a9e236851d51392
henriquesqs <henriquesqs@hotmail.com>
2020-12-13 10:46:05 -0300

    Commit to start github actions

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,7 +4,7 @@ import "./assets/styles/global.css";
 
 // Main component of our application
 function App() {
-    return <Routes />; // Returning routes to our single-page application
+    return <Routes />; // Returning routes to our single-page app
 }
 
 export default App;
